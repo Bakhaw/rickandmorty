bad25501570c35ce501c5649390750959e8d1ab2
Faissal <fhattou@simplon.co>
2019-03-18 03:28:07 +0100

    Initial commit :fire: :fire:

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <header>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+  );
+}
