c9bfa0ad08e9872dfb23784c00f3868494bf9db2
Faissal <fhattou@simplon.co>
2019-03-25 16:32:03 +0100

    Splitted code and use of <Collection/> components

--- src/screens/Home/Characters.js
+++ src/screens/Home/Characters.js
@@ -1,65 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import api from '../../api';
 
-import CharacterCard from '../../components/CharacterCard';
+import Collection from '../../components/Collection';
 import Loader from '../../components/Loader';
-import Pagination from '../../components/Pagination';
 
-const CharactersList = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 360px);
-  gap: 60px 0;
-  justify-content: center;
-  padding: 20px 0;
-`;
-
-const CharactersListItem = styled.li`
-  margin: 0 30px;
+const Wrapper = styled.div`
+  padding: 30px 0;
 `;
 
 function Characters() {
-  const [characters, setCharacters] = React.useState([]);
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const [totalPages, setTotalPages] = React.useState({});
-  const [isLoading, setLoading] = React.useState(false);
+  const [isEndOfList, setIsEndOfList] = useState(false);
+  const [characters, setCharacters] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setLoading] = useState(false);
 
   const getAllCharacters = async page => {
     await setLoading(true);
-    await api.methods
-      .GET_ALL_CHARACTERS(page)
-      .then(({ characters, totalPages }) => {
-        setCharacters(characters);
-        setTotalPages(totalPages);
-      });
+
+    const newCharacters = await api.methods.GET_ALL_CHARACTERS(page);
+
+    if (!newCharacters) {
+      await setLoading(false);
+      await setIsEndOfList(true);
+      return;
+    }
+
+    await setCharacters([...characters, ...newCharacters]);
+
     await setLoading(false);
   };
 
-  const handleChangePage = e => {
-    setCurrentPage(e.selected + 1);
+  const handleChangePage = () => {
+    if (isEndOfList) return;
+    setCurrentPage(currentPage + 1);
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     getAllCharacters(currentPage);
   }, [currentPage]);
 
-  if (isLoading) return <Loader />;
+  if (characters.length === 0) return <Loader height='fullscreen' />;
 
+  const kiss = '\u{1F618}';
   return (
-    <React.Fragment>
-      <CharactersList>
-        {characters.map((character, index) => (
-          <CharactersListItem key={index}>
-            <CharacterCard {...character} />
-          </CharactersListItem>
-        ))}
-      </CharactersList>
-      <Pagination
-        currentPage={currentPage}
-        handleChangePage={handleChangePage}
-        totalPages={totalPages}
-      />
-    </React.Fragment>
+    <Wrapper>
+      <Collection list={characters} onBottom={handleChangePage} />
+      {isLoading && <Loader />}
+      {isEndOfList && <p>You know every single character now {kiss}</p>}
+    </Wrapper>
   );
 }
 
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -13,7 +13,7 @@ function Home() {
   };
 
   React.useEffect(() => {
-    triggerSplash();
+    // triggerSplash();
   }, []);
 
   if (showSplash) return <Splash />;
